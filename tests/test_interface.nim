9ce0ffd954b64d46132448d817650b5b0c4e1d1e
Ryan Cotter <ryanc@lastyard.com>
2020-05-15 09:02:33 +0800

    Comments, cleanup, extra test

--- tests/test_interface.nim
+++ tests/test_interface.nim
@@ -1,7 +1,10 @@
+## TODO: Split these up
+## Maybe see if I can store a base state of the process before each tests runs and roll back after
 import osproc, streams, os
 import unittest
 
 proc getResponse(inStream, outStream: var Stream, lines: seq[string] = @[]): string =
+  ## Write all lines in `lines` to inStream and read the result
   for line in lines:
     inStream.writeLine(line)
   inStream.flush()
@@ -9,7 +12,7 @@ proc getResponse(inStream, outStream: var Stream, lines: seq[string] = @[]): str
 
 suite "Interface Tests":
 
-  test "Test Output":
+  test "Test Standard Syntax works":
     var process = startProcess(
       "bin/inim",
       workingDir = "",
@@ -26,6 +29,22 @@ suite "Interface Tests":
       "a"
     ]
     require getResponse(inputStream, outputStream, defLines) == "A == type string"
+
+    let typeLines = @[
+      "type B = object",
+      "c: string",
+      "",
+      "B"
+    ]
+    require getResponse(inputStream, outputStream, typeLines) == "B == type B"
+    # This could be improved
+    require getResponse(inputStream, outputStream, @["B.c"]) == "string == type string"
+
+    let varLines = @[
+      """var g = B(c: "C")""",
+      "g"
+    ]
+    require getResponse(inputStream, outputStream, varLines) == """(c: "C") == type B"""
 
     inputStream.writeLine("quit")
     inputStream.flush()
