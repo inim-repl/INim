422584d3b407163742046c60f1323d83c0adf7c1
Tristram Oaten <tristram@oaten.name>
2020-06-30 10:27:04 +0100

    fix paths

--- setupci.sh
+++ setupci.sh
@@ -1,18 +1,15 @@
 #! /bin/bash
 
 # This script borrowed from the Nimble project - Thanks!
-
-export GITBIN=$HOME/.choosenim/git/bin
+export BRANCH=devel
 export PATH=$HOME/.nimble/bin:$GITBIN:$PATH
 
 if ! type -P choosenim &> /dev/null; then
   echo "Fresh install"
-  mkdir -p $GITBIN
 
   export CHOOSENIM_CHOOSE_VERSION="$BRANCH --latest"
   curl https://nim-lang.org/choosenim/init.sh -sSf > init.sh
   sh init.sh -y
-  cp $HOME/.nimble/bin/choosenim$EXT $GITBIN/.
 
 else
   echo "Already installed"
